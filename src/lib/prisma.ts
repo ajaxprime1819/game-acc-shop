1611f35d6021f7c6c68e1eb842c4f3ba6df6b652
ajaxprime1819 <cuongdzzzzzzz09@gmail.com>
2026-07-28 17:55:53 +0700

    Create prisma.ts

---
+++ src/lib/prisma.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  // eslint-disable-next-line
+  var __db: PrismaClient | undefined;
+}
+
+export const prisma = global.__db || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") global.__db = prisma;
